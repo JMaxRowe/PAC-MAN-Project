8ca4e04dff63fb5835fbfcfe94cb45bba13ffe5e
JMaxRowe <jmaxrowe@gmail.com>
2025-06-30 11:31:41 +0100

    pacman moves w/object collision and added pellet class

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,185 @@
 const layout = [
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-  1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
-  1, 0, 1, 0, 0, 3, 0, 1, 0, 1,
-  1, 0, 1, 0, 1, 1, 0, 1, 0, 1,
-  1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 1, 1, 0, 1, 0, 1,
+    1, 0, 1, 0, 0, 3, 0, 1, 0, 1,
+    1, 0, 1, 0, 1, 1, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 ];
 
+
 let pacmanIndex
+let playerSpeed = 200
+let currentSpeed = playerSpeed
+let currentDirection;
 
 const mapWidth = 10;
+const mapHeight = 12
+const mapArea = mapHeight * mapWidth;
 const grid = document.getElementById("grid");
 const cells = []; 
 
 function createMap() {
-  layout.forEach((cellType, index) => {
-    const cell = document.createElement("div");
-    cell.dataset.index = index;
-    cell.innerText = index;
+    layout.forEach((cellType, index) => {
+        const cell = document.createElement("div");
+        cell.dataset.index = index;
+        cell.innerText = index;
 
-    if (cellType === 1) {
-      cell.classList.add("wall");
-    } else if (cellType === 0) {
-      cell.classList.add("path");
-    } else if(cellType === 3) {
+        if (cellType === 1) {
+        cell.classList.add("wall");
+        } else if (cellType === 0) {
         cell.classList.add("path");
-        cell.classList.add('pacman')
-        pacmanIndex = index;
+        cell.classList.add("pellet");
+        } else if(cellType === 3) {
+            cell.classList.add("path");
+        }
 
-    }
+        grid.appendChild(cell);
+        cells.push(cell);
+    });
+}
 
-    grid.appendChild(cell);
-    cells.push(cell);
-  });
+function addPacMan(){
+    layout.forEach((cellType, index) =>{
+        if (cellType === 3) {
+            cells[index].classList.add("pacman");
+            pacmanIndex = index;
+}
+    })
 }
 
 function init(){
     createMap()
+    addPacMan()
 }
 
+function movePlayer(e){
+    console.log(e);
+    if(e.code === "KeyW"){
+        startMovement("up");
+        console.log("up")}
+    else if(e.code === "KeyA"){
+        startMovement("left");
+        console.log("left")}
+    else if(e.code === "KeyS"){
+        startMovement("down");
+        console.log("down")}
+    else if(e.code === "KeyD"){
+        startMovement("right");
+        console.log("right")}
+    else{console.log("not valid input")}
+} 
+
+function startMovement(dir){
+    if (dir === "up"){
+        if (!cells[pacmanIndex-mapWidth].classList.contains("wall")){
+            clearInterval(movementInterval)
+            movementInterval = 0
+            moveUp()
+        }
+        else(console.log("wall in the way"))
+    }
+    else if (dir === "left"){
+        if (!cells[pacmanIndex-1].classList.contains("wall")){
+            clearInterval(movementInterval)
+            movementInterval = 0
+            moveLeft()
+        }
+        else{console.log("wall in the way")}
+    }
+    else if(dir === "down"){
+        if (!cells[pacmanIndex + mapWidth].classList.contains("wall")){
+            clearInterval(movementInterval)
+            movementInterval = 0
+            moveDown()
+        }
+        else{console.log("wall in the way")}
+    }
+    else if (!cells[pacmanIndex+1].classList.contains("wall")){
+            clearInterval(movementInterval)
+            movementInterval = 0
+            moveRight()
+        }
+        else{console.log("wall in the way")}
+}
+
+let movementInterval;
+function moveUp(){
+    if (!movementInterval){
+        movementInterval = setInterval(() =>{
+            let newPosition = pacmanIndex - mapWidth
+            if (!cells[newPosition].classList.contains("wall")){
+                cells[pacmanIndex].classList.remove("pacman")
+                cells[newPosition].classList.add("pacman")
+                pacmanIndex = newPosition;}
+            else{
+                clearInterval(movementInterval);
+                movementInterval=0;
+            }
+        }, playerSpeed)
+    }
+}
+
+function moveDown(){
+    if (!movementInterval){
+        movementInterval = setInterval(() =>{
+            let newPosition = pacmanIndex + mapWidth
+            if (!cells[newPosition].classList.contains("wall")){
+                cells[pacmanIndex].classList.remove("pacman")
+                cells[newPosition].classList.add("pacman")
+                pacmanIndex = newPosition;}
+                else{
+                    clearInterval(movementInterval);
+                    movementInterval = 0;
+                }
+        }, playerSpeed)
+    }
+}
+
+function moveLeft(){
+    if (!movementInterval){
+        movementInterval = setInterval(() =>{
+            let newPosition = pacmanIndex - 1
+            if (!cells[newPosition].classList.contains("wall")){
+                cells[pacmanIndex].classList.remove("pacman")
+                cells[newPosition].classList.add("pacman")
+                pacmanIndex = newPosition;}
+                else{
+                    clearInterval(movementInterval)
+                    movementInterval = 0
+                }
+        }, playerSpeed)
+    }
+}
+
+function moveRight(){
+    if (!movementInterval){
+        movementInterval = setInterval(() =>{
+            let newPosition = pacmanIndex + 1
+            if (!cells[newPosition].classList.contains("wall")){
+                cells[pacmanIndex].classList.remove("pacman")
+                cells[newPosition].classList.add("pacman")
+                pacmanIndex = newPosition;}
+                else{
+                    clearInterval(movementInterval)
+                    movementInterval = 0
+                }
+        }, playerSpeed)
+    }
+}
+
+
+
+
+
+
+
+document.addEventListener("keydown", movePlayer)
+
+
 init()
+
 
 
 
@@ -91,3 +230,15 @@ init()
 
 
 // createMap()
+
+// let timerInterval;
+// function startMovement (e, dir){
+//     if(!timerInterval){
+//         clearInterval(timerInterval)
+//         timerInterval = setInterval(() => {
+//             console.log("move")
+//             if(dir === "up"){
+//                 console.log(pacmanIndex - mapWidth)
+//             }
+//     }, playerSpeed)}
+// }
